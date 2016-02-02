d52878afc9bde82b67bd8555c114ef666c844fce
bellettif <bellettif@gmail.com>
2016-02-01 23:26:18 -0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,34 @@
+# on Edision we will benchmark you against the default vendor-tuned BLAS. The compiler wrappers handle all the linking. If you wish to compare with other BLAS implementations, check the NERSC documentation.
+# This makefile is intended for the GNU C compiler. To change compilers, you need to type something like: "module swap PrgEnv-pgi PrgEnv-gnu" See the NERSC documentation for available compilers.
+
+CC = cc 
+OPT = -O1
+CFLAGS = -Wall -std=gnu99 $(OPT)
+LDFLAGS = -Wall -framework Accelerate
+# librt is needed for clock_gettime
+#LDLIBS = -lrt
+
+targets = benchmark-naive benchmark-blocked benchmark-blas benchmark-opt
+objects = benchmark.o dgemm-naive.o dgemm-blocked.o dgemm-blas.o opt-dgemm-blocked.o
+
+.PHONY : default
+default : all
+
+.PHONY : all
+all : clean $(targets)
+
+benchmark-naive : benchmark.o dgemm-naive.o 
+	$(CC) -o $@ $^ $(LDLIBS) $(LDFLAGS)
+benchmark-blocked : benchmark.o dgemm-blocked.o
+	$(CC) -o $@ $^ $(LDLIBS) $(LDFLAGS)
+benchmark-blas : benchmark.o dgemm-blas.o
+	$(CC) -o $@ $^ $(LDLIBS) $(LDFLAGS)
+benchmark-opt : benchmark.o opt-dgemm-blocked.o
+	$(CC) -o $@ $^ $(LDLIBS) $(LDFLAGS)
+
+%.o : %.c
+	$(CC) -c $(CFLAGS) $<
+
+.PHONY : clean
+clean:
+	rm -f $(targets) $(objects)
---
+++ opt-dgemm-blocked.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+
+const char* dgemm_desc = "Simple blocked dgemm.";
+
+#if !defined(BLOCK_SIZE)
+#define BLOCK_SIZE 41
+#endif
+
+#define min(a,b) (((a)<(b))?(a):(b))
+
+/* This auxiliary subroutine performs a smaller dgemm operation
+ *  C := C + A * B
+ * where C is M-by-N, A is M-by-K, and B is K-by-N. */
+inline static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C, double* transpose_buffer)
+{
+  int i, j, k;
+  register double cij;
+  double *b_ptr;
+  /* For each row i of A */
+  for (i = 0; i < M; ++i) {
+    for (k = 0; k < K; ++k) {
+      transpose_buffer[k] = A[i + k * lda];
+    }
+    /* For each column j of B */
+    for (j = 0; j < N; ++j) {
+      /* Compute C(i,j) */
+      cij = C[i + j * lda];
+      b_ptr = B + j * lda;
+      for (k = 0; k < K; ++k) {
+        cij += transpose_buffer[k] * b_ptr[k];
+      }
+      C[i + j * lda] = cij;
+    }
+  }
+}
+
+/* This routine performs a dgemm operation
+ *  C := C + A * B
+ * where A, B, and C are lda-by-lda matrices stored in column-major format. 
+ * On exit, A and B maintain their input values. */  
+void square_dgemm (int lda, double* A, double* B, double* C)
+{
+  int i, j, k, M, N, K;
+  double* transpose_buffer = (double *) malloc(BLOCK_SIZE * sizeof(double));
+  /* For each block-row of A */ 
+  for (i = 0; i < lda; i += BLOCK_SIZE)
+    /* For each block-column of B */
+    for (j = 0; j < lda; j += BLOCK_SIZE)
+      /* Accumulate block dgemms into block of C */
+      for (k = 0; k < lda; k += BLOCK_SIZE)
+      {
+	/* Correct block dimensions if block "goes off edge of" the matrix */
+	M = min (BLOCK_SIZE, lda-i);
+	N = min (BLOCK_SIZE, lda-j);
+	K = min (BLOCK_SIZE, lda-k);
+
+	/* Perform individual block dgemm */
+	do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda, transpose_buffer);
+      }
+  free(transpose_buffer);
+}
