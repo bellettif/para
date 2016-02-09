38946872f791b1dec10f3947f89f8a684890180c
Liang RuoChen <Vincent@airbears2-10-142-16-42.airbears2.1918.berkeley.edu>
2016-02-08 17:05:33 -0800

    updated the inner most loop to be 8X4, great performance incrementgit add -u

--- dgemm-44blocked.c
+++ dgemm-44blocked.c
@@ -1,4 +1,6 @@
 #include <emmintrin.h> 
+#include <immintrin.h> //AVX intrinsics
+#include <stdio.h>
 const char* dgemm_desc = "Optimized version for n %4 == 0.";
 
 #define BLOCK_SIZE 128
@@ -21,6 +23,26 @@ void copyA(int k, double *A, int lda, double *copy)
   }
 }
 
+// This subroutine copy an 8 by k block of A
+void copyA8(int k, double *A, int lda, double *copy)
+{
+  int j;
+  for(j = 0; j<k; j++)
+  {
+    double *a_ptr = A+j*lda;
+    *copy = *a_ptr;
+    *(copy+1) = *(a_ptr+1);
+    *(copy+2) = *(a_ptr+2);
+    *(copy+3) = *(a_ptr+3);
+    *(copy+4) = *(a_ptr+4);
+    *(copy+5) = *(a_ptr+5);
+    *(copy+6) = *(a_ptr+6);
+    *(copy+7) = *(a_ptr+7);
+
+    copy += 8;
+  }
+}
+
 void copyB(int k, double *B, int ldb, double *copy)
 {
   int i;
@@ -36,7 +58,7 @@ void copyB(int k, double *B, int ldb, double *copy)
   }
 }
 
-void update4X4(int k, double *A, int lda,  double *B, int ldb, double *C, int ldc)
+void update4X4(int k, double *A, int lda, double *B, int ldb, double *C, int ldc)
 {
   int p;
   __m128d c_00_c_10, c_01_c_11, c_02_c_12, c_03_c_13,
@@ -88,6 +110,62 @@ void update4X4(int k, double *A, int lda,  double *B, int ldb, double *C, int ld
   _mm_storeu_pd(C+2+ldc*3, c_23_c_33);
 }
 
+// Test another subroutine that updates an 8 by 4 block of C at a time
+void update8X4(int k, double *A, int lda, double* B, int ldb, double *C, int ldc)
+{
+  int p;
+  __m256d c_00_30, c_01_31, c_02_32, c_03_33, c_40_70, c_41_71, c_42_72, c_43_73,
+          a_0p_3p, a_4p_7p,
+          b_p0, b_p1, b_p2, b_p3;
+
+  c_00_30 = _mm256_loadu_pd(C);
+  c_01_31 = _mm256_loadu_pd(C+ldc);
+  c_02_32 = _mm256_loadu_pd(C+ldc*2);
+  c_03_33 = _mm256_loadu_pd(C+ldc*3);
+  c_40_70 = _mm256_loadu_pd(C+4);
+  c_41_71 = _mm256_loadu_pd(C+4+ldc);
+  c_42_72 = _mm256_loadu_pd(C+4+ldc*2);
+  c_43_73 = _mm256_loadu_pd(C+4+ldc*3);
+
+
+  for(p=0; p<k; p++)
+  {
+    // Load a
+    a_0p_3p = _mm256_loadu_pd(A);
+    a_4p_7p = _mm256_loadu_pd(A+4);
+    A += lda;
+    //printf("Succesfully loaded a col of A.\n");
+    // Load b
+    b_p0 = _mm256_broadcast_sd(B);     
+    b_p1 = _mm256_broadcast_sd(B+1); 
+    b_p2 = _mm256_broadcast_sd(B+2); 
+    b_p3 = _mm256_broadcast_sd(B+3); 
+
+    B += 4; 
+
+    // First four rows of C updated once
+    c_00_30 = _mm256_add_pd(c_00_30, _mm256_mul_pd(a_0p_3p, b_p0));
+    c_01_31 = _mm256_add_pd(c_01_31, _mm256_mul_pd(a_0p_3p, b_p1));
+    c_02_32 = _mm256_add_pd(c_02_32, _mm256_mul_pd(a_0p_3p, b_p2));
+    c_03_33 = _mm256_add_pd(c_03_33, _mm256_mul_pd(a_0p_3p, b_p3));
+
+    // Last four rows of C updated once
+    c_40_70 = _mm256_add_pd(c_40_70, _mm256_mul_pd(a_4p_7p, b_p0));
+    c_41_71 = _mm256_add_pd(c_41_71, _mm256_mul_pd(a_4p_7p, b_p1));
+    c_42_72 = _mm256_add_pd(c_42_72, _mm256_mul_pd(a_4p_7p, b_p2));
+    c_43_73 = _mm256_add_pd(c_43_73, _mm256_mul_pd(a_4p_7p, b_p3));
+  }
+
+  _mm256_storeu_pd(C, c_00_30);
+  _mm256_storeu_pd(C+ldc, c_01_31);
+  _mm256_storeu_pd(C+ldc*2, c_02_32);
+  _mm256_storeu_pd(C+ldc*3, c_03_33);
+  _mm256_storeu_pd(C+4, c_40_70);
+  _mm256_storeu_pd(C+4+ldc, c_41_71);
+  _mm256_storeu_pd(C+4+ldc*2, c_42_72);
+  _mm256_storeu_pd(C+4+ldc*3, c_43_73);
+}
+
 void do_block(int lda, int m, int n, int k, double *A, double *B, double *C, int need_to_packB)
 {
   int i, j;
@@ -96,9 +174,14 @@ void do_block(int lda, int m, int n, int k, double *A, double *B, double *C, int
   for (j=0; j<n; j+=4){        /* Loop over the columns of C, unrolled by 4 */
     if (need_to_packB)
       copyB(k, B+j*lda, lda, &packedB[j*k]);
-    for (i=0; i<m; i+=4){        /* Loop over the rows of C */
-      if (j == 0) copyA(k, A+i, lda, &packedA[i*k]);
-      update4X4(k, &packedA[i*k], 4, &packedB[j*k], k, C+i+j*lda, lda);
+    for (i=0; i<m; i+=8){        /* Loop over the rows of C */
+      if (j == 0) copyA8(k, A+i, lda, &packedA[i*k]);
+      // for(int p = 0; p < 8*k; p++)
+      // {
+      //   printf("%f ", packedA[p]);
+      //   if (p % k == k - 1) printf("\n");
+      // }
+      update8X4(k, &packedA[i*k], 8, &packedB[j*k], k, C+i+j*lda, lda);
     }
   }
 }
