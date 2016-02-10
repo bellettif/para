a88d7ae097cd915efad2f8ca097f66d827072bef
Liang RuoChen <Vincent@airbears2-10-142-16-95.airbears2.1918.berkeley.edu>
2016-02-09 19:05:10 -0800

    The version now works for arbitrary n

--- dgemm-44blocked.c
+++ dgemm-44blocked.c
@@ -3,25 +3,11 @@
 #include <stdio.h>
 const char* dgemm_desc = "Optimized version for n %4 == 0.";
 
-#define BLOCK_SIZE 128
+#define BLOCK_SIZE 256
 #define nb 1000
+#define DEBUG 0
 
 #define min( i, j ) ( (i)<(j) ? (i): (j) )
-void copyA(int k, double *A, int lda, double *copy)
-{
-  int j;
-  for(j=0; j<k; j++)
-  {  /* loop over columns of A */
-    double *a_ptr = A+j*lda;
-
-    *copy = *a_ptr;
-    *(copy+1) = *(a_ptr+1);
-    *(copy+2) = *(a_ptr+2);
-    *(copy+3) = *(a_ptr+3);
-
-    copy += 4;
-  }
-}
 
 // This subroutine copy an 8 by k block of A
 void copyA8(int k, double *A, int lda, double *copy)
@@ -58,57 +44,6 @@ void copyB(int k, double *B, int ldb, double *copy)
   }
 }
 
-void update4X4(int k, double *A, int lda, double *B, int ldb, double *C, int ldc)
-{
-  int p;
-  __m128d c_00_c_10, c_01_c_11, c_02_c_12, c_03_c_13,
-          c_20_c_30, c_21_c_31, c_22_c_32, c_23_c_33,
-          a_0p_a_1p, a_2p_a_3p,
-          b_p0, b_p1, b_p2, b_p3; 
-
-  c_00_c_10 = _mm_loadu_pd(C);   
-  c_01_c_11 = _mm_loadu_pd(C+ldc);
-  c_02_c_12 = _mm_loadu_pd(C+ldc*2); 
-  c_03_c_13 = _mm_loadu_pd(C+ldc*3); 
-  c_20_c_30 = _mm_loadu_pd(C+2);   
-  c_21_c_31 = _mm_loadu_pd(C+2+ldc);  
-  c_22_c_32 = _mm_loadu_pd(C+2+ldc*2);   
-  c_23_c_33 = _mm_loadu_pd(C+2+ldc*3); 
-
-  for(p=0; p<k; p++)
-  {
-    a_0p_a_1p = _mm_load_pd(A);
-    a_2p_a_3p = _mm_load_pd(A+2);
-    A += 4;
-
-    b_p0 = _mm_load1_pd(B);     
-    b_p1 = _mm_load1_pd(B+1); 
-    b_p2 = _mm_load1_pd(B+2); 
-    b_p3 = _mm_load1_pd(B+3); 
-
-    B += 4;
-
-    /* First row and second rows */
-    c_00_c_10 = _mm_add_pd(c_00_c_10, _mm_mul_pd(a_0p_a_1p, b_p0));
-    c_01_c_11 = _mm_add_pd(c_01_c_11, _mm_mul_pd(a_0p_a_1p, b_p1));
-    c_02_c_12 = _mm_add_pd(c_02_c_12, _mm_mul_pd(a_0p_a_1p, b_p2));
-    c_03_c_13 = _mm_add_pd(c_03_c_13, _mm_mul_pd(a_0p_a_1p, b_p3));
-
-    /* Third and fourth rows */
-    c_20_c_30 = _mm_add_pd(c_20_c_30, _mm_mul_pd(a_2p_a_3p, b_p0));
-    c_21_c_31 = _mm_add_pd(c_21_c_31, _mm_mul_pd(a_2p_a_3p, b_p1));
-    c_22_c_32 = _mm_add_pd(c_22_c_32, _mm_mul_pd(a_2p_a_3p, b_p2));
-    c_23_c_33 = _mm_add_pd(c_23_c_33, _mm_mul_pd(a_2p_a_3p, b_p3));
-  }
-  _mm_storeu_pd(C, c_00_c_10);
-  _mm_storeu_pd(C+ldc, c_01_c_11);
-  _mm_storeu_pd(C+ldc*2, c_02_c_12);
-  _mm_storeu_pd(C+ldc*3, c_03_c_13);
-  _mm_storeu_pd(C+2, c_20_c_30);
-  _mm_storeu_pd(C+2+ldc, c_21_c_31);
-  _mm_storeu_pd(C+2+ldc*2, c_22_c_32);
-  _mm_storeu_pd(C+2+ldc*3, c_23_c_33);
-}
 
 // Test another subroutine that updates an 8 by 4 block of C at a time
 void update8X4(int k, double *A, int lda, double* B, int ldb, double *C, int ldc)
@@ -166,21 +101,110 @@ void update8X4(int k, double *A, int lda, double* B, int ldb, double *C, int ldc
   _mm256_storeu_pd(C+4+ldc*3, c_43_73);
 }
 
+// This is the routine to handle edge cases, needs to implement
+void dgemm_edge(int lda, int m, int n, int k, double *A, double *B, double* C)
+{
+  if (m == 0 || n == 0) return;
+   // This is for debug purpose:
+   if (DEBUG) printf("Updaing edge case m = %d, n = %d, k = %d.\n", m, n, k);
+   // This routine will do a little optimization by updating 2X2 blocks 
+  int r, c, p;
+  __m128d c_00_10, c_01_11,
+          a_0p_1p,
+          b_p0, b_p1;
+  for(r = 0; r < m/2*2; r+=2)
+  {
+    for(c = 0; c < n/2*2; c+=2)
+    {
+      // This is updating a 2X2 block at (r, c)
+      c_00_10 = _mm_loadu_pd(C+r+c*lda);
+      c_01_11 = _mm_loadu_pd(C+r+(c+1)*lda);
+
+      // Summing up over k
+      for(p = 0; p < k; p++)
+      {
+        a_0p_1p = _mm_loadu_pd(A+r+p*lda);
+        b_p0 = _mm_load1_pd(B+p+c*lda);
+        b_p1 = _mm_load1_pd(B+p+(c+1)*lda);
+
+        c_00_10 = _mm_add_pd(c_00_10, _mm_mul_pd(a_0p_1p, b_p0));
+        c_01_11 = _mm_add_pd(c_01_11, _mm_mul_pd(a_0p_1p, b_p1));
+      }
+
+      _mm_storeu_pd(C+r+c*lda, c_00_10);
+      _mm_storeu_pd(C+r+(c+1)*lda, c_01_11);
+    }
+  }
+
+  // This requires accessing the whole matrix B, so there is still room for optimization
+  if (m % 2)
+  {
+    // Need to update the last row
+    for(c = 0; c < n; c++)
+    {
+      for(p = 0; p < k; p ++)
+      {
+        C[m-1+c*lda] += A[m-1+p*lda]*B[p+c*lda];
+      }
+    }
+
+    if (n % 2)
+    {
+      for(r = 0; r < m-1; r++)
+      {
+        for(p = 0; p < k; p ++)
+        {
+          C[r+(n-1)*lda] += A[r+p*lda]*B[p+(n-1)*lda];
+        }
+      }
+    }
+  }else if (n % 2)
+  {
+    for(r = 0; r < m; r++)
+    {
+      for(p = 0; p < k; p ++)
+      {
+        C[r+(n-1)*lda] += A[r+p*lda]*B[p+(n-1)*lda];
+      }
+    }
+  }
+
+}
+
 void do_block(int lda, int m, int n, int k, double *A, double *B, double *C, int need_to_packB)
 {
+  if (DEBUG) printf("Doing block m = %d, n = %d, k = %d\n", m, n, k);
   int i, j;
   double packedA[m*k], packedB[BLOCK_SIZE*nb]; 
 
-  for (j=0; j<n; j+=4){        /* Loop over the columns of C, unrolled by 4 */
+  for (j=0; j<n/4*4; j+=4){        /* Loop over the columns of C, unrolled by 4 */
     if (need_to_packB)
       copyB(k, B+j*lda, lda, &packedB[j*k]);
-    for (i=0; i<m; i+=8){        /* Loop over the rows of C */
+    for (i=0; i<m/8*8; i+=8){        /* Loop over the rows of C */
       if (j == 0) copyA8(k, A+i, lda, &packedA[i*k]);
       update8X4(k, &packedA[i*k], 8, &packedB[j*k], k, C+i+j*lda, lda);
     }
   }
 
-
+  if (m%8!=0 && n%4 == 0)
+  {
+    // Need to update a bottom row block
+    int row_index = m/8*8;  
+    dgemm_edge(lda, m - row_index, n, k, A+row_index, B, C+row_index);
+  }
+  else if (m%8!=0 && n%4 != 0)
+  {
+    // Need to update a bottom row block and a tailing column block
+    int row_index = m/8*8, col_index = n/4*4;
+    dgemm_edge(lda, m-row_index, n, k, A+row_index, B, C+row_index);
+    dgemm_edge(lda, row_index, n-col_index, k, A, B+lda*col_index, C+lda*col_index);
+  }
+  // Here we rule out the case that m%8 == 0 and n%4 != 0 cause that's not possible
+  else if (m%8 == 0 && n%4!=0)
+  {
+    int col_index = n/4*4;
+    dgemm_edge(lda, m, n-col_index, k, A, B+lda*col_index, C+lda*col_index);
+  }
 }
 
 
